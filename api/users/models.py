1449edbeaf1eb5b3645d2b79d60c9e578e0a5368
Daniyar Bakhtiyar <daniyar0507050711@gmail.com>
2023-04-03 09:59:19 +0600

    replaced backend with api

---
+++ api/users/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+
+    def __str__(self) -> str:
+        return self.username
+    
+    def __repr__(self) -> str:
+        return self.username
