d149e9e4b5aae2eed546ba30d68f1fbfab96e702
Alvanerle <zzhumash03@gmail.com>
2023-04-05 17:36:41 +0600

    models added

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Status)
+admin.site.register(Category)
+admin.site.register(Movie)
+admin.site.register(Event)
+admin.site.register(Ticket)
+admin.site.register(Cinema)
