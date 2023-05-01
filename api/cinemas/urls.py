c09f759025c920ee12a03b7bd9e45154d15ee462
Alvanerle <zzhumash03@gmail.com>
2023-05-01 18:01:52 +0600

    event by cinema added

--- api/cinemas/urls.py
+++ api/cinemas/urls.py
@@ -4,5 +4,6 @@ from rest_framework import routers
 from .views import *
 
 urlpatterns = [
-    path('by_time/', EventByTimeViewSet.as_view())
+    path('by_time/', EventByTimeViewSet.as_view()),
+    path('by_cinema/', EventByCinemaViewSet.as_view())
 ]
--- api/cinemas/views.py
+++ api/cinemas/views.py
@@ -19,3 +19,10 @@ class EventByTimeViewSet(APIView):
         serializer_class = EventSerializer(queryset, many=True)
         return Response(serializer_class.data)
 
+
+@permission_classes((permissions.AllowAny,))
+class EventByCinemaViewSet(APIView):
+    def get(self, request):
+        queryset = Event.objects.all().filter(date=parse_date(request.data.get('date')), movie=request.data.get('movie_id')).order_by('cinema', 'start_time')
+        serializer_class = EventSerializer(queryset, many=True)
+        return Response(serializer_class.data)
